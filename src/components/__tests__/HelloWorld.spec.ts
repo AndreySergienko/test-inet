126f4aacc4e876e0bdd019035a09c3b52a92924e
Andrey Sergineko <getmessage@gmail.com>
2024-07-02 00:18:16 +0500

    check vitest

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -6,7 +6,6 @@ import HelloWorld from '../HelloWorld.vue'
 describe('HelloWorld', () => {
   it('renders properly', () => {
     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect('test').toContain('name')
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
